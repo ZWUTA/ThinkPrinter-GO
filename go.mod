ec137d1d1229a28e479cf72bd86595ec312db92e
sky96111 <sky96111@outlook.com>
2023-06-29 19:31:03 +0800

    :sparkles:feat：更稳定的浏览器打开方式

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module thinkPrinter
 go 1.20
 
 require (
-	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
-	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	github.com/golang-jwt/jwt/v5 v5.0.0
+	github.com/mattn/go-sqlite3 v1.14.17
+	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8
 )
+
+require golang.org/x/sys v0.0.0-20210616045830-e2b7044e8c71 // indirect
--- tools/open_explorer.go
+++ tools/open_explorer.go
@@ -1,27 +1,17 @@
 package tools
 
 import (
-	"fmt"
-	"os/exec"
-	"runtime"
+	"github.com/pkg/browser"
+	"log"
 )
 
 // OpenExplorer 打开本地浏览器
 func OpenExplorer(url string) {
 	url = "http://" + url
-	var cmd *exec.Cmd
-	switch runtime.GOOS {
-	case "windows":
-		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
-	case "darwin": // macOS
-		cmd = exec.Command("open", url)
-	default: // Linux
-		cmd = exec.Command("xdg-open", url)
-	}
-
-	if err := cmd.Start(); err != nil {
-		fmt.Printf("请手动打开浏览器并访问 %s 进行操作\n", url)
+	err := browser.OpenURL(url)
+	if err != nil {
+		log.Printf("请手动打开浏览器并访问 %s 进行操作\n", url)
 	} else {
-		fmt.Println("打开浏览器成功, 请在网页中进行操作")
+		log.Println("打开浏览器成功, 请在网页中进行操作")
 	}
 }

b35b56cb05b94d152922480be21cd6e8834c0cee
sky96111 <sky96111@outlook.com>
2023-06-29 23:11:47 +0800

    refactor: scheme username唯一约束

--- static/init.sql
+++ static/init.sql
@@ -1,14 +1,15 @@
 CREATE TABLE "users"
 (
-    id         integer           not null
-        primary key autoincrement,
+    id         integer
+        constraint id_pk primary key,
     created_at datetime,
     updated_at datetime,
     deleted_at datetime,
-    username   text              not null,
+    username   text
+        constraint username_uk unique
+                                 not null,
     password   text              not null,
     name       text,
     number     text,
     vip        numeric default 0 not null
 )
-

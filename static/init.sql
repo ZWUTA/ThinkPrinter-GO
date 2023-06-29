a27148637d6871058b9ba4680d42231d8ca2ee86
sky96111 <sky96111@outlook.com>
2023-06-29 22:16:30 +0800

    refactor: scheme修改为适合gorm的格式

--- static/init.sql
+++ static/init.sql
@@ -1,11 +1,14 @@
-create table main.users
+CREATE TABLE "users"
 (
-    uid       integer           not null
-        constraint users_pk
-            primary key autoincrement,
-    sname     TEXT,
-    snumber   TEXT,
-    uname     TEXT              not null,
-    upassword TEXT              not null,
-    vip       integer default 0 not null
-);
+    id         integer           not null
+        primary key autoincrement,
+    created_at datetime,
+    updated_at datetime,
+    deleted_at datetime,
+    username   text              not null,
+    password   text              not null,
+    name       text,
+    number     text,
+    vip        numeric default 0 not null
+)
+

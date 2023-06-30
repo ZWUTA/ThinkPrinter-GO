23028a1cdd58e8e2bbbeaf5ed99d7296ac6e2b46
sky96111 <sky96111@outlook.com>
2023-06-30 17:01:30 +0800

    fix: 修正文件路径
    TODO: 使用go embed嵌入静态文件

--- web/index.go
+++ web/index.go
@@ -8,7 +8,7 @@ import (
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("./web/index.html")
+	tmpl, err := template.ParseFiles("static/index.html")
 	if err != nil {
 		log.Println(err)
 	}
